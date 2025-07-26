fa3e9bba1c43e954fc642b793e0adbb8953a4bf0
omusinov <a_musinov@yahoo.com>
2025-07-25 21:01:39 -0400

    tailwind setup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='flex flex-col'>
-      <div>One</div>
-      <div>Two</div>
-      <div>Three</div>
+      <div className='border-1 m-2'>One</div>
+      <div className='border-1 m-2'>Two</div>
+      <div className='border-1 m-2'>Three</div>
     </div>
   )
 }
